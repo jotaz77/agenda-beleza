72be3bed096a0aedb8f82dbf20123a1d479673af
Almir Cavalcante <almir2cavalcante@gmail.com>
2026-05-08 06:11:16 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,5 @@
-window.supabaseClient = window.supabase.createClient(
-  "https://bjaurtrqrasznuptthyf.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJqYXVydHJxcmFzem51cHR0aHlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5MDkwNTIsImV4cCI6MjA5MzQ4NTA1Mn0.iCQyvzAJykzgLO4kZkJZ8qNbv3549rG_JuwcVEZDrKA"
-);
+// supabase.js
+const supabaseUrl = "https://bjaurtrqrasznuptthyf.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJqYXVydHJxcmFzem51cHR0aHlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5MDkwNTIsImV4cCI6MjA5MzQ4NTA1Mn0.iCQyvzAJykzgLO4kZkJZ8qNbv3549rG_JuwcVEZDrKA";
+
+window.supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
