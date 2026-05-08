8cc06b98afb9d59f82f302f3de973414e187bfd2
Almir Cavalcante <almir2cavalcante@gmail.com>
2026-05-08 06:36:51 -0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,5 +0,0 @@
-// supabase.js
-const supabaseUrl = "https://bjaurtrqrasznuptthyf.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJqYXVydHJxcmFzem51cHR0aHlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5MDkwNTIsImV4cCI6MjA5MzQ4NTA1Mn0.iCQyvzAJykzgLO4kZkJZ8qNbv3549rG_JuwcVEZDrKA";
-
-window.supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
